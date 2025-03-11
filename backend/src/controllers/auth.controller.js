636898c68dc8f001c738f31162692c33ee584edd
Deepak-0119288 <deeking0123@gmail.com>
2025-03-11 18:19:26 +0530

    token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -76,7 +76,7 @@ const login = async (req, res) => {
 
 const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
+    res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in Logout Controller");
