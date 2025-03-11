ea82e580d1833869c929d94c6f481f20ca967bd1
Deepak-0119288 <deeking0123@gmail.com>
2025-03-11 21:55:12 +0530

    again

--- backend/src/lib/token.js
+++ backend/src/lib/token.js
@@ -7,7 +7,6 @@ const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
     sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000, 
     path: "/", 
